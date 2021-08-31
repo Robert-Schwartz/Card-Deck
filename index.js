4a6ca9c881009248735e42520259f0f297a183c7
Robert Schwartz <bschwartz23@gmail.com>
2021-08-31 16:13:16 -0700

    randomize and shuffle functionality

--- index.js
+++ index.js
@@ -15,18 +15,40 @@ const values = [
 	"K",
 ];
 
-function getDeck()
-{
+//Create a new Deck Object
+function getDeck() {
 	var deck = new Array();
 
-	for(var i = 0; i < suits.length; i++)
-	{
-		for(var x = 0; x < cards.length; x++)
-		{
-			var card = {Value: cards[x], Suit: suits[i]};
+	for (var i = 0; i < suits.length; i++) {
+		for (var x = 0; x < cards.length; x++) {
+			var card = { Value: cards[x], Suit: suits[i] };
 			deck.push(card);
 		}
 	}
 
 	return deck;
 }
+
+// Create Shuffle Function
+function shuffle() {
+	// for 1000 turns
+	// switch the values of two random cards
+	for (var i = 0; i < 1000; i++) {
+		var location1 = Math.floor(Math.random() * deck.length);
+		var location2 = Math.floor(Math.random() * deck.length);
+		var tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
+
+	renderDeck();
+}
+
+//Load Functions
+function load() {
+	deck = getDeck();
+	shuffle();
+}
+
+window.onload = load;
